5b6ed8189e919617bdf35e02f79b611c9b9e5f02
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-04-29 17:50:29 +0200

    Creating questions and answers in js

--- app.js
+++ app.js
@@ -11,22 +11,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
             function createQuestion(index) {
                 const questionElement = document.createElement('div');
-                questionElement.id = 'question';
+                questionElement.id = 'exercise';
                 quiz.appendChild(questionElement);
 
                 let header = document.createElement('h2');
                 header.innerText = 'Question ' + (index + 1) + ' :';
                 questionElement.appendChild(header);
 
-                var task = document.createElement('p');
+                let task = document.createElement('p');
                 task.innerText = data.questions[index].question;
                 questionElement.appendChild(task);
 
-
+                let answ = createAnswer(index);
+                questionElement.appendChild(answ);
 
                 return questionElement;
             }
-            createQuestion(0);
+
+            function createAnswer(index) {
+                let ul = document.createElement('ul');
+                let li;
+                let input = '';
+                let options;
+
+                for (var i = 0; i < data.questions[index].answers.length; i++) {
+                    li = document.createElement('li');
+                    options = document.createElement('p');
+                    input = document.createElement('input');
+                    input.type = "checkbox";
+                    input.name = "answer";
+                    input.value = i;
+                    options.innerText = data.questions[index].answers[i].answer;
+                    options.appendChild(input);
+                    li.appendChild(options);
+                    ul.appendChild(li);
+                }
+                return ul;
+            }
+
+
+            createQuestion(3);
 
 
         })
